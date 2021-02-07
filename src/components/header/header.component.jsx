56ac706e950cf294e5c57885f36289592a088c64
Dave <dave.cadelina@outlook.com>
2021-02-07 16:07:12 +1100

    added cart dropdown component and set new cart state and linked header with the state, and also set action method for the state in redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,9 @@ import { ReactComponent as Logo} from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 import { connect } from 'react-redux'
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     
 return (
         <div className="header">
@@ -28,12 +29,17 @@ return (
                 }
                 <CartIcon />
             </div>
+            {
+                hidden ? null :
+                <CartDropdown/>
+            }
         </div>
         )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
