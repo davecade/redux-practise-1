90e536cf31c63a76b2290f4c95e4dc393a8aece1
Dave <dave.cadelina@outlook.com>
2021-02-12 02:23:24 +1100

    added cart item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './cart-dropdown.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
+import cartItem from '../cart-item/cart-item.component'
 
 const CartDropdown = () => (
     <div className="cart-dropdown">
